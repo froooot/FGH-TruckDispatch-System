dab9f17cf90dfbbe073c63dfea2d38bb4d6177d3
froooot <ghukasyangev@gmail.com>
2021-09-22 17:06:32 +0400

    - Environment variable check added.

--- app.py
+++ app.py
@@ -41,8 +41,8 @@ sqliteConnection = sqlite3.connect("./finance.db", check_same_thread=False)
 sqliteConnection.row_factory = sqlite3.Row
 
 # Make sure API key is set
-# if not os.environ.get("API_KEY"):
-#     raise RuntimeError("API_KEY not set")
+if not os.environ.get("google_API_KEY"):
+    raise RuntimeError("google_API_KEY not set")
 
 
 @app.route("/")
